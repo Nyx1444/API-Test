eca9abd73c39d2433f2347f53db8772d0025a92f
Nyx1444 <kantapongmini@gmail.com>
2024-09-04 01:24:46 +0700

    Fix CORS initialization and install cors module

--- api/users.js
+++ api/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const serverless = require('serverless-http');
 const path = require('path');
-const cors = require('cors');
-app.use(cors());
+
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public'))); // ใช้ไฟล์ static จาก public
 
